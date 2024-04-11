755bc89672276596cd3118a38eb2b522b5ffb5c4
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-04-11 16:02:55 -0300

    configurado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VariaveisAmbiente.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
